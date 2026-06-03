ed725c979827ce8039f52b60dcb7ffc9ccca0c87
delipex <delipefamasceno@gmail.com>
2026-06-03 20:38:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Sistema carregado.");
+
+window.addEventListener('scroll',()=>{
+document.querySelector('.header').style.background =
+window.scrollY > 50 ? 'rgba(0,0,0,.9)' : 'rgba(0,0,0,.65)';
 });
